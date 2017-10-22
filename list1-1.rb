a0e3c6a936e6c88beba8cc6756fc9c88d48f1681
Tomohisa Uchida <uddystyle@gmail.com>
2017-10-22 16:24:26 +0900

    Finished list1-1 to list1-4

---
+++ list1-1.rb
@@ -0,0 +1,8 @@
+class Sample
+  def say
+    puts "Hello, World!!"
+  end
+end
+
+sample = Sample.new
+sample.say
---
+++ list1-4.rb
@@ -0,0 +1,12 @@
+p "hello".class
+
+p String.ancestors
+
+p 10.class
+p Fixnum.ancestors
+
+p true.class
+p TrueClass.ancestors
+
+p nil.class
+p NilClass.ancestors
